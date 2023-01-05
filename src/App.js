3abec0c8e1a11092a74ef95ff5395e6464764434
CarlieMartece <carliemartece@gmail.com>
2023-01-05 18:13:50 +0000

    add Header component and first logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <Link to="/">
+        <img
+          alt="CM-redblackwhite-hex-logo"
+          src={require("./images/layout/logo-CM-141.png")}
+        />
+      </Link>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ey up!</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
