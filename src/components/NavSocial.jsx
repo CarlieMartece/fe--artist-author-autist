e1e8fcf390b0ca4d10df6d9b3ac78494ad3ec9b7
CarlieMartece <carliemartece@gmail.com>
2023-01-06 18:13:30 +0000

    add placeholder hexes for future platforms

--- src/components/NavSocial.jsx
+++ src/components/NavSocial.jsx
@@ -2,25 +2,39 @@ import { ExternalLink } from "react-external-link";
 
 export default function NavSocial() {
   const socialArray = [
-    ["https://www.patreon.com/CarlieMartece", "patreon", "pt"],
     ["https://www.linkedin.com/in/carlie-martece/", "linked-in", "li"],
     ["https://github.com/CarlieMartece", "github", "gh"],
+    ["https://www.patreon.com/CarlieMartece", "patreon", "pt"],
   ];
 
   return (
-    <nav className="nav-section" id="NavSocial">
-      {socialArray.map((platform) => {
-        return (
-          <ExternalLink href={platform[0]}>
+    <nav className="NavSocial">
+      <div className="nav-section" id="nav-social-one">
+        {socialArray.map((platform) => {
+          return (
+            <ExternalLink href={platform[0]}>
+              <div className="hexagon">
+                <img
+                  alt={`${platform[1]}-link`}
+                  src={require(`../images/layout/logo-${platform[2]}-100.png`)}
+                />
+              </div>
+            </ExternalLink>
+          );
+        })}
+      </div>
+      <div className="nav-section" id="nav-social-two">
+        {socialArray.map(() => {
+          return (
             <div className="hexagon">
               <img
-                alt={`${platform[1]}-link`}
-                src={require(`../images/layout/logo-${platform[2]}-100.png`)}
+                alt="placeholder"
+                src={require(`../images/layout/placeholder-hex-100.png`)}
               />
             </div>
-          </ExternalLink>
-        );
-      })}
+          );
+        })}
+      </div>
     </nav>
   );
 }
