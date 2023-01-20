d635c08723f21710b516c2dc3a117d0b0ec5b017
CarlieMartece <carliemartece@gmail.com>
2023-01-20 13:07:14 +0000

    render and style pi grid

--- src/components/PiGrid.jsx
+++ src/components/PiGrid.jsx
@@ -1,11 +1,32 @@
-const { greyObj, piToColour } = require("../utils.js");
+//import { useEffect, useState } from "react";
+import { greyObj, piToColour } from "../utils";
 
 export default function PiGrid() {
+    // const [piArray, setPiArray] = useState([]);
+    // useEffect(() => {
+    //     piToColour(10000, greyObj).then((piPairs) => {
+    //         setPiArray(piPairs);
+    //     })
+    // }, []);
+    
+    const piArray = piToColour(10000, greyObj)
+    
 
-    console.log(piToColour(100, greyObj))
-
-    return (
-        <h2>Pi Grid</h2>
-    )
-
+  return (
+    <main>
+      <div id="PiGrid">
+        {piArray.map((piPair) => {
+          return (
+            <div
+              className="piSquare"
+              key={piPair[0]}
+              style={{
+                backgroundColor: `${piPair[1]}`,
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
